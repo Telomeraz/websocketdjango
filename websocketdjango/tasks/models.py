65c357a781833d862576fa75922b9e3053ba2810
Telomeraz <huseyinemre_armagan@hotmail.com>
2021-03-20 19:10:43 +0300

    Created Task model

--- websocketdjango/tasks/models.py
+++ websocketdjango/tasks/models.py
@@ -1,3 +1,20 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+
+    STATUSES = (
+        ("STARTED", "Started"),
+        ("IN PROGRESS", "In progress"),
+        ("FINISHED", "Finished"),
+    )
+
+    name = models.CharField("Task name", max_length=255, blank=True)
+
+    status = models.CharField("Status", max_length=255, choices=STATUSES)
+
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.status
