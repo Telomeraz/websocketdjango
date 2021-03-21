213f9baa2df364db2bef955850b72ea74b2043dc
Telomeraz <huseyinemre_armagan@hotmail.com>
2021-03-21 16:56:16 +0300

    Created routing

--- websocketdjango/websocketdjango/routing.py
+++ websocketdjango/websocketdjango/routing.py
@@ -1,5 +1,11 @@
-from channels.routing import ProtocolTypeRouter
+from django.urls import path
+from django.conf.urls import url
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from tasks.consumers import TaskConsumer
 
 application = ProtocolTypeRouter({
-
+    "websocket": URLRouter([
+        path("tasks/", TaskConsumer()),
+    ]),
 })
