fe81a85979e138af4f13f45fd7ae677940678e35
Telomeraz <huseyinemre_armagan@hotmail.com>
2021-03-21 16:56:43 +0300

    Fixed asgi for websocket

--- websocketdjango/websocketdjango/asgi.py
+++ websocketdjango/websocketdjango/asgi.py
@@ -1,8 +1,10 @@
 import os
 
-from django.core.asgi import get_asgi_application
+from channels.routing import get_default_application
+
+import django
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "websocketdjango.settings")
-
-application = get_asgi_application()
+django.setup()
+application = get_default_application()
